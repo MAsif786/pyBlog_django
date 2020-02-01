5c5cc01c5662fc229f1e4ce25f90ca8bc8a88dc2
asif <snew3110@gmail.com>
2020-02-02 03:11:17 +0500

    connect to psql db

--- pyBlog/settings.py
+++ pyBlog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pyBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pyblog',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
